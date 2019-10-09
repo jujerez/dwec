7d745288a3af203570ab5edfa135b93a20e1c36e
Juan Antonio Jerez <juanantonio.jerez@iesdonana.org>
2019-10-09 19:34:03 +0200

    RegExp matricula españa

---
+++ tema3/ejercicios-regexp/e20.js
@@ -0,0 +1,11 @@
+/**20. Valida una matrícula moderna */
+// Las consonantes que se utilizan para las matriculaciones son: B, C, D, F, G, H, J, K, 
+// L, M, N, P, R, S, T, V, W, X, Y y Z; y se eliminan la Ñ y 
+// la Q por posible confusión con la N y la O.
+var pat = /^([0-9]){4}-([B-D]|[F-H]|[J|N]|P|[R-T]|[V-Z]){3}$/
+var mat;
+
+do {
+    mat = prompt("Introduzca una matricula");
+    pat.test(mat) ? alert("Matricula correcta") : alert("Matricula incorrecta, intentelo de nuevo")
+} while (!pat.test(mat));
