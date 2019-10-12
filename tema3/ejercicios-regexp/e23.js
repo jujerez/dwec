383c5bac85f1f09d00c2f36117d217138dc0a347
Juan Antonio Jerez <juanantonio.jerez@iesdonana.org>
2019-10-12 19:08:21 +0200

    RegExp la ultima palabra de una frase

---
+++ tema3/ejercicios-regexp/e23.js
@@ -0,0 +1,5 @@
+/** . Buscar la ultima palabra de cada frase*/
+var frase = prompt("Introduzca una frase")
+var pat = /\w*\S$/gm;
+var array = pat.exec(frase);
+array != null ? alert(`La ultima es: ${array}`) : alert(`La ultima es un espacio`);
