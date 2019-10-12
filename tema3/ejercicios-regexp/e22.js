4130f1d4501a933ebc91185ed7ffb597257fa9b9
Juan Antonio Jerez <juanantonio.jerez@iesdonana.org>
2019-10-12 19:33:05 +0200

    mejora para frase de una sola palabra

--- tema3/ejercicios-regexp/e22.js
+++ tema3/ejercicios-regexp/e22.js
@@ -1,5 +1,5 @@
 /** . Buscar la primera palabra de cada frase*/
 var frase = prompt("Introduzca una frase")
-var pat = /\w* /;
+var pat = /(\w* )|(\w*)/;
 var array = pat.exec(frase);
-alert(`La primera palabra es: ${array}`);
+alert(`La primera palabra es ${array[0]}`);
