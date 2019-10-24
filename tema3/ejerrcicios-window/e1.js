08a494d35d80b5861a5dde5c5483325b7436e6a4
Juan Antonio Jerez <juanantonio.jerez@iesdonana.org>
2019-10-24 20:01:34 +0200

    Ejercicios objeto window

---
+++ tema3/ejerrcicios-window/e1.js
@@ -0,0 +1,11 @@
+/**Crea una página web que realice las siguientes funciones:
+Imprimir, Abrir ventana (figura2), Abrir ventana y detenerla, Cerrar ventana nueva, Dar foco
+ventana nueva, Quitar foco ventana nueva, Scroll absoluto (0, 1000) y Scroll relativo (0, 10).  */
+
+imprimir.onclick = function() {
+    window.print();
+}
+
+abrir.onclick = function() {
+    window.open(" ","ventanita nueva","width=500, height=500");
+}
